2cb209fe4f04841c62e4f73e15759a2527a45fe2
Omer Alrwais <omer.alrwais@cgu.edu>
2013-06-03 13:21:19 -0700

    navigation between screens added

--- src/cgu/edu/ist380/er/ListClass.java
+++ src/cgu/edu/ist380/er/ListClass.java
@@ -1,9 +1,13 @@
 package cgu.edu.ist380.er;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListClass extends Activity {
@@ -18,10 +22,20 @@ public class ListClass extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.list);
 		hos = (ListView) findViewById(R.id.HosList);
 		arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, hospital);
 		hos.setAdapter(arrayAdapter);
+		
+		Button mainNext = (Button) findViewById(R.id.detail);
+	       
+	       mainNext.setOnClickListener(new OnClickListener() {
+	           public void onClick(View v) {
+	               Intent i = new Intent();
+	               i.setClassName("cgu.edu.ist380.er", "cgu.edu.ist380.er.Detail");
+	               startActivity(i);
+	           }
+	       });
 	}
 
 	@Override
--- src/cgu/edu/ist380/er/MainActivity.java
+++ src/cgu/edu/ist380/er/MainActivity.java
@@ -2,7 +2,11 @@ package cgu.edu.ist380.er;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button mainNext = (Button) findViewById(R.id.finder);
+	       
+	       mainNext.setOnClickListener(new OnClickListener() {
+	           public void onClick(View v) {
+	               Intent i = new Intent();
+	               i.setClassName("cgu.edu.ist380.er", "cgu.edu.ist380.er.ListClass");
+	               startActivity(i);
+	           }
+	       });
 	}
 
 	@Override
